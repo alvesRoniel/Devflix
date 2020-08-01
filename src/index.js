b2c4873f5bcdca75007b4c3972f6104d028eb846
Roniel S. Alves <roniel.alves@gmail.com>
2020-08-01 15:59:08 -0300

    Finalizando o carrousel e o slider.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import CadastrarVideo from './pages/cadastro/video';
+import CadastrarCategoria from './pages/cadastro/categoria';
+
+
+const Pagina404 = () => (<div>Página 404</div>);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={ Home } exact/>
+      <Route path="/cadastro/video" component={ CadastrarVideo }/>
+      <Route path="/cadastro/categoria" component={ CadastrarCategoria }/>
+      <Route component={ Pagina404 }/>  
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
