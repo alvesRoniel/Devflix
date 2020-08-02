62b44c2a11c94a06b3034d6d596d5dfe4137dbd0
Roniel S. Alves <roniel.alves@gmail.com>
2020-08-02 00:51:29 -0300

    Iniciando o formulário de cadastro de categorias.

--- src/pages/cadastro/categoria/index.js
+++ src/pages/cadastro/categoria/index.js
@@ -1,14 +1,77 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PageDefault from '../../../components/PageDefault';
 import { Link } from 'react-router-dom';
 
 function CadastrarCategoria() {
+
+    const [categorias, setCategorias] = useState(['Teste']);
+    const [nomeDaCategoria, setNomeDaCategoria] = useState();
+
+    function AlterarValorDoInput(infosDoEvento) {
+        setNomeDaCategoria(infosDoEvento.target.value);
+    }
+
     return (
         <PageDefault>
-            <h1>Cadastro de Categoria</h1>
-            
+            <h1>Cadastro de Categoria: {nomeDaCategoria}</h1>
+
+            <form onSubmit={function handleSubmint(infosDoEvento) {
+                infosDoEvento.preventDefault();
+
+                setCategorias([
+                    ...categorias,
+                    nomeDaCategoria
+                ]);
+            }}>
+                <div>
+                    <label>
+                        Nome da Categoria:
+                    <input type="text"
+                            value={nomeDaCategoria}
+                            onChange={AlterarValorDoInput}
+                        />
+                    </label>
+                </div>
+
+                <div>
+                    <label>
+                        Descrição:
+                    <input type="text"
+                            value={nomeDaCategoria}
+                            onChange={AlterarValorDoInput}
+                        />
+                    </label>
+                </div>
+
+                <div>
+                    <label>
+                        Cor:
+                    <input type="color"
+                            value={nomeDaCategoria}
+                            onChange={AlterarValorDoInput}
+                        />
+                    </label>
+                </div>
+
+                <button>
+                    Cadastar
+                </button>
+
+            </form>
+
+            <ul>
+                {categorias.map((categoria, indice) => {
+                    return (
+                        <li key={`${categoria}${indice}`}>
+                            {categoria}
+                        </li>
+                    )
+                })}
+            </ul>
+
+
             <Link to="/">
-               Ir para a home
+                Ir para a home
             </Link>
         </PageDefault>
     );
